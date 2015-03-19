93e2b017f16ee0d618b9a416dff47f724d64d4a2
Marsel Zaripov <marszaripov@gmail.com>
2015-03-19 16:38:04 +0300

    fix import error in tasks

--- msync/tasks.py
+++ msync/tasks.py
@@ -1,9 +1,10 @@
 from celery import task
-from msync.queryset import BatchQuery
 
 
 @task.task()
 def sync_task(parent_sync_cls, tasks):
+    from .queryset import BatchQuery
+
     with BatchQuery(parent_sync_cls) as b:
         for t in tasks:
             t(b)
